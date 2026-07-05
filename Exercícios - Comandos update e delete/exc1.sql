74294a4e5730fb5162b6995d167195cf2123b86e
Thales Santos <thalesferreiramontenegro@gmail.com>
2026-07-05 13:09:50 -0300

    Resolvidos os exercícios 1 ao 6 sobre os comandos update e delete

---
+++ Exercícios - Comandos update e delete/exc1.sql
@@ -0,0 +1,8 @@
+insert into cliente (idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+values (16, 'Maicon', '12349596421', '1234', '1965-10-10', 'F', 'Empresário', null, null, null, null, null, 'Florianópolis', 'PR');
+
+insert into cliente (idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+values (17, 'Getúlio', null, '4631', null, 'F', 'Estudante', 'Brasileira', 'Rua Central', '343', 'Apartamento', 'Centro', 'Curitiba', 'SC');
+
+insert into cliente (idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+values (18, 'Sandra', null, null, null, 'M', 'Professor', 'Italiana', null, '12', 'Bloco A', null, null, null);
---
+++ Exercícios - Comandos update e delete/exc4.sql
@@ -0,0 +1,2 @@
+update cliente set genero = 'F', profissao = 'Professora', numero = '123'
+where idcliente = 18;
